2960d4e6babf39433b849c294b7c161d835a5719
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-07 11:53:09 +0200

    Create draw2image.py

---
+++ draw2image.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from PIL import Image, ImageDraw
+
+img = Image.new('RGBA', (200, 200), 'white')    
+idraw = ImageDraw.Draw(img)
+
+idraw.rectangle((10, 10, 100, 100), fill='blue')
+ 
+img.save('rectangle.png')
