7035009cd327bd7cc99db8018212338e11a860fe
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-07 11:50:23 +0200

    Create convert2png.py

---
+++ convert2png.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import sys
+
+try:
+    tatras = Image.open("tatras.jpg")
+
+except IOError:
+    print("Unable to load image")
+    sys.exit(1)
+
+tatras.save('tatras.png', 'png')  
